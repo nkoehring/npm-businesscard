c8a14fbc525b573ec6ddd94a79a6bd5a84a5bf5e
Norman Köhring <norman.koehring@ypog.law>
2025-12-18 15:17:11 +0100

    all around update

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 #! /usr/bin/env node
 
-const { blueBright, bold, white, greenBright, redBright } = require("colorette")
-const boxen = require("boxen")
+import { blueBright, bold, white, greenBright, red, underline } from "colorette"
+import boxen from "boxen"
 
 const boxOptions = {
   padding: 1,
   margin: 0,
-  borderStyle: 'double'
+  borderStyle: 'double',
 }
 
-const work = white(`Senior Consultant @ ${bold('Wunder')}${bold(redBright('dog'))}`)
+const work = white(`Principal Frontend Engineer @ ${bold(red('YPOG'))}`)
 const twitter = blueBright('https://twitter.com/koehr_in')
 const github = blueBright('https://github.com/nkoehring')
+const forge = blueBright('https://git.koehr.ing/')
 
 const npm = blueBright('https://https://www.npmjs.com/~koehr')
 const npx = white('npx koehr')
-const www = blueBright('https://koehr.in')
-const blog = blueBright('https://koehr.tech')
+const www = blueBright('https://koehr.ing')
 
 function withHeading(heading, text) {
   const coloredHeading = white(bold(`     ${heading}:`))
   return `${coloredHeading} ${text}`
 }
 
-const headline = `${white(bold('                    Norman Köhring '))}`
+const headline = `${white(bold(underline('              Norman Köhring  ')))}`
 
 const output = [
   headline,
+  '',
   withHeading('   Work', work),
-  withHeading('Twitter', twitter),
+  withHeading('    WWW', www),
+  withHeading('  Forge', forge),
   withHeading(' Github', github),
   withHeading('    NPM', npm),
-  withHeading('    WWW', www),
-  withHeading('   Blog', blog),
   withHeading('   Card', npx),
 ].join('\n')
 
